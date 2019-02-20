cba5c179f3c118323790ba704c5950c4843766e2
gaoji <gaojieqi888@163.com>
2019-02-20 19:22:39 +0800

    111

--- face-1/EADA.py
+++ face-1/EADA.py
@@ -754,6 +754,8 @@ def fobj(hot,cold,T,split,structure_info,heat_load,cold_utility,hot_utility):
     for ii in range(Nc):
         HU+=hot_utility[ii]
     c_energy=HU*c_hu+CU*c_cu
+    print("hot utility:",HU)
+    print ("cold utility:",CU)
     print("operation cost:",c_energy)
     print("fix cost:",c_capital)
     c_global=c_energy+c_capital
